b0624c19430e847c1c8c3f26462c013752786753
SergioCarboBen <sercarben@alu.edu.gva.es>
2025-12-01 16:01:29 +0100

    Update utilidades.py

--- utilidades.py
+++ utilidades.py
@@ -6,19 +6,15 @@ def comprobar_ganador(x, y, tablero):
 
     ##########################################
     # Miramos hacia la izquierda y derecha
-    victoria_lateral = True
-
-    # TODO: comprobacion
-    pass
+    if tablero[0][y] == tablero[1][y] and tablero[1][y] == tablero[2][y]:
+        victoria_lateral = True
     
     if victoria_lateral:
         return True
     ##########################################
     # Miramos hacia abajo
-    victoria_columna = True
-
-    # TODO: comprobacion
-    pass
+    if tablero[x][0] == tablero[x][1] and tablero[x][1] == tablero[x][2]:
+        victoria_columna = True
 
     if victoria_columna:
         return True
@@ -39,6 +35,17 @@ def mostrar_tablero(tablero):
 # - --> 0
 def convertir_tablero(tablero):
     nuevo_tablero = []
+    for fila in tablero:
+        fila_traducida = []
+        fila_separada = tuple(fila)
+        for casilla in fila_separada:
+            if casilla == 'X': 
+                fila_traducida.append(1)
+            elif casilla == 'O':
+                fila_traducida.append(-1)
+            else:
+                fila_traducida.append(0)
+        nuevo_tablero.append(fila_traducida)
 
     ##########################################
     #TODO: Convertirlo a una lista de listas
